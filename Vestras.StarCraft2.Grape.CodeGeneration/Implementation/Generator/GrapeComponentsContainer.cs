224c7c6e74d6392c71ac938add3fc3bdd7bef1a9
s3rius <JBansen@gmx.de>
2011-02-18 18:29:49 +0100

    These two too!

---
+++ Vestras.StarCraft2.Grape.CodeGeneration/Implementation/Generator/GrapeComponentsContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Vestras.StarCraft2.Grape.Core;
+
+namespace Vestras.StarCraft2.Grape.CodeGeneration.Implementation.Generation {
+    internal class GrapeComponentsContainer {
+
+        private List<string> variables = new List<string>();
+
+        public void AddVariable(GrapeVariable v) {
+            string fullName = v.Name;
+            GrapeEntity e = v.Parent;
+
+            while (e != null) {
+                fullName = e.GetPotentialEntityName() + "__" + fullName;
+                e = e.Parent;
+            }
+
+            if (v.Length == 0) {
+                fullName = v.Type + " " + fullName + ";" + Environment.NewLine;
+            } else {
+                fullName = v.Type + "[" + v.Length + "] " + fullName + ";" + Environment.NewLine;
+            }
+
+
+            variables.Add(fullName);
+        }
+    }
+}
