ce53d604e0bb44e79b7beba4e5c5a3710e40b15c
Ars?ne von Wyss <arsene@vonwyss.ch>
2011-04-03 23:41:07 +0200

    Created small test project to check the grammar/parser

---
+++ Grape2Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using Vestras.StarCraft2.Grape.Core;
+using Vestras.StarCraft2.Grape.Core.Ast;
+using Vestras.StarCraft2.Grape.Core.Implementation;
+
+namespace Grape2Test {
+	class Program {
+		static void Main(string[] args) {
+			GrapeParser parser = new GrapeParser();
+			GrapeAst ast = parser.Parse(@"c:\temp\Grape2.txt", false, false);
+			foreach (GrapeEntity entity in ast.Children) {
+				Debug.WriteLine("Entity: "+entity.GetType().Name);
+				foreach (GrapeEntity child in entity.GetChildren<GrapeExpression>()) {
+					Debug.WriteLine(" -> child: "+child.GetType().Name);
+				}
+			}
+			Console.ReadKey(true);
+		}
+	}
+}
